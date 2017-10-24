5fedf276cef7add27912050c0efe17a8cdbc64bc
Jonathan Stites <mail@jonstites.com>
2017-10-24 18:38:05 -0500

    add a script for playing game

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+#!/usr/bin/env python3
+from distutils.core import setup
 
-config = {
-    'description': 'A stupid blackjack game',
-    'author': 'Jonathan Stites',
-    'author_email': 'contact@jonstites.com.',
-    'version': '0.1',
-    'install_requires': ['pytest'],
-    'packages': [],
-    'scripts': [],
-    'name': 'blackjack'
-}
-
-setup(**config)
+setup(name= 'blackjack',
+    description= 'A stupid blackjack game',
+    author= 'Jonathan Stites',
+    author_email= 'contact@jonstites.com.',
+    version= '0.1',
+    install_requires= ['pytest'],
+    packages= ['blackjack'],
+    url='https://github.com/jonstites/blackjack',
+    scripts= ['bin/play_blackjack.py'],
+)
